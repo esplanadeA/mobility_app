26c8b28a412c4e96e1f62db0496058b2442e4747
esplanadeA <anna.li.dev@gmail.com>
2023-06-21 12:29:43 +1200

    image stack exported and stack add to navbar

---
+++ src/assets/images.js
@@ -0,0 +1,8 @@
+import banner from './images/banner.png';
+import Logo1 from './images/Logo1.png';
+import Logo from './images/Logo.png';
+import gym from './icons/gym.png';
+import equipment from './icons/equipment.png';
+import target from './icons/target.png';
+
+export default { banner, Logo1, Logo, gym, equipment, target };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Stack } from '@mui/material';
+
+import { images } from '../assets';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <Stack>
+      <Link to="/">
+        <img
+          src={images.Logo}
+          alt="logo"
+          style={{
+            width: '48px',
+            height: '48px',
+          }}
+        />
+      </Link>
+      <Stack>
+        <Link to="/">Home</Link>
+        <a href="#exercise">Exercise</a>
+      </Stack>
+    </Stack>
+  );
 };
 
 export default Navbar;
