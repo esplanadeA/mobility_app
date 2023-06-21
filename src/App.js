1dc0ba3933c3efcbae033b29b69b05be00d88373
esplanadeA <anna.li.dev@gmail.com>
2023-06-21 12:11:32 +1200

    components and route added to doc;

--- src/App.js
+++ src/App.js
@@ -2,9 +2,22 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Box } from '@mui/material';
 import './App.css';
+import ExerciseDetail from './container/ExcerciseDetail';
+import Home from './container/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Box width="400px">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
+  );
 };
 
 export default App;
