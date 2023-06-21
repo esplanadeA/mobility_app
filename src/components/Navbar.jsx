9356ccdee99cb2e17137e414cf06ed25b995292c
esplanadeA <anna.li.dev@gmail.com>
2023-06-21 13:01:22 +1200

    styling for navbar in place with mui

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,20 +6,53 @@ import { images } from '../assets';
 
 const Navbar = () => {
   return (
-    <Stack>
-      <Link to="/">
+    <Stack
+      direction="row"
+      justifyContent="space-around"
+      sx={{
+        gap: { sm: '123px', xs: '40px' },
+        mt: { sm: '32px', xs: '20px' },
+        justifyContent: 'none',
+      }}
+      px="20px"
+    >
+      <Link
+        to="/"
+        style={{
+          textDecoration: 'none',
+          color: '#3A1212',
+          borderBottom: '3px solid #FF2625',
+        }}
+      >
         <img
           src={images.Logo}
           alt="logo"
-          style={{
-            width: '48px',
-            height: '48px',
-          }}
+          style={{ width: '48px', height: '48px', margin: '0px 20px' }}
         />
       </Link>
-      <Stack>
-        <Link to="/">Home</Link>
-        <a href="#exercise">Exercise</a>
+      <Stack
+        direction="row"
+        gap="40px"
+        fontFamily="Alegreya"
+        fontSize="24px"
+        alignItems="flex-end"
+      >
+        <Link
+          to="/"
+          style={{
+            textDecoration: 'none',
+            color: '#3A1212',
+            borderBottom: '3px solid #FF2625',
+          }}
+        >
+          Home
+        </Link>
+        <a
+          href="#exercise"
+          style={{ textDecoration: 'none', color: '#3A1212' }}
+        >
+          Exercise
+        </a>
       </Stack>
     </Stack>
   );
