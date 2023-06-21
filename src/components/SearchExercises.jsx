7eb5766edbc5de326d83580522c6c92763f1574d
esplanadeA <anna.li.dev@gmail.com>
2023-06-21 14:26:48 +1200

    file sturcture updated

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -1,7 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Box, Stack, Button, Typography, TextField } from '@mui/material';
 
 const SearchExercises = () => {
-  return <div>SearchExercises</div>;
+  const [search, setSearch] = useState('');
+  const handleSearch = async (e) => {
+    if (search) {
+    }
+  };
+  return (
+    <>
+      <Stack
+        alignItems="center"
+        mt="37px"
+        justifyContent="center"
+        p="20px"
+      ></Stack>
+      <Typography
+        fontWeight={700}
+        sx={{ fontSize: { lg: '44px', xs: '30px' } }}
+        mb="49px"
+        textAlign="center"
+      >
+        Awesome Exercises You <br /> Should Know
+      </Typography>
+      <Box position="relative" mb="72px">
+        <TextField
+          height="76px"
+          sx={{
+            input: { fontWeight: '700', border: 'none', borderRadius: '4px' },
+            width: { lg: '1170px', xs: '350px' },
+            backgroundColor: '#fff',
+            borderRadius: '40px',
+          }}
+          onChange={(e) => setSearch(e.target.value.toLowerCase())}
+          placeholder="Search Exercises"
+          type="text"
+          value={search}
+        />
+        <Button
+          className="search-btn"
+          sx={{
+            bgcolor: '#FF2625',
+            color: '#fff',
+            textTransform: 'none',
+            width: { lg: '173px', xs: '80px' },
+            height: '56px',
+            position: 'absolute',
+            right: '0px',
+            fontSize: { lg: '20px', xs: '14px' },
+          }}
+          onClick={handleSearch}
+        >
+          Search
+        </Button>
+      </Box>
+      <Box sx={{ position: 'relative', width: '100%', p: '20px' }}></Box>
+    </>
+  );
 };
 
 export default SearchExercises;
