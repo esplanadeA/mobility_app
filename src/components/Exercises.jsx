7408426c93d3dece1429df626d137505737c6dd5
esplanadeA <anna.li.dev@gmail.com>
2023-07-26 12:43:32 +1200

    seraching part working, map is not functioning, no exercise data passing through yet

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -9,6 +9,15 @@ import { exerciseOptions, fetchData } from '../utils/fetchData';
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
   <Box id="exercises" sx={{ mt: { lg: '109px' } }} mt="50px" p="20px">
     <Typography variant="h3">showing result</Typography>
+    <Stack
+      direction="row"
+      sx={{ gap: { lg: '107px', xs: '50px' } }}
+      flexWrap="wrap"
+      justifyContent="center"
+    >
+      {console.log(exercises)}
+      <p>{exercises.map((exercise, idx) => exercise.name)}</p>
+    </Stack>
   </Box>;
 };
 
