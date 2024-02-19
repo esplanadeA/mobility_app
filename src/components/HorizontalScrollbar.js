be6d2bfdabca5abc9f42d7c591bac4db6d9a6bab
esplanadeA <anna.li.dev@gmail.com>
2024-02-19 21:26:00 +1300

    exercisecard add;

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
 import { Box, Typography } from '@mui/material';
+import ExerciseCard from './ExerciseCard';
 
 // import ExerciseCard from './ExerciseCard';
 import BodyPart from './BodyPart';
@@ -28,16 +29,15 @@ const RightArrow = () => {
   );
 };
 
-const HorizontalScrollbar = ({ data, setBodyPart, bodyPart }) => (
+const HorizontalScrollbar = ({ data, bodyParts, setBodyPart, bodyPart }) => (
   <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
     {data.map((item) => (
-      <Box
-        key={item.id || item}
-        itemID={item.id || item}
-        title={item.id || item}
-        m="0 40px"
-      >
-        <BodyPart item={item} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+      <Box key={item.id || item} itemId={item.id || item} title={item.id || item} m="0 40px">
+        {bodyParts ? (
+          <BodyPart item={item} setBodyPart={setBodyPart} bodyPart={bodyPart} />
+        ) : (
+          <ExerciseCard exercise={item} />
+        )}
       </Box>
     ))}
   </ScrollMenu>
