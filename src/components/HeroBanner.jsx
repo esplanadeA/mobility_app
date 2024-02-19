3624804eb9654198c591f46d79f7cb68e2487e77
esplanadeA <anna.li.dev@gmail.com>
2024-02-19 21:38:19 +1300

    hero banner update for better image present

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -5,16 +5,11 @@ import { images } from '../assets';
 
 const HeroBanner = () => {
   return (
-    <Box sx={{ mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' } }} p="20px">
+    <Box sx={{ mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' } }} position="relative" p="20px">
       <Typography color="#FF2625" fontWeight="600" fontSize="26px">
         Fitness Club
       </Typography>
-      <Typography
-        fontWeight={700}
-        sx={{ fontSize: { lg: '44px', xs: '40px' } }}
-        mb="23px"
-        mt="30px"
-      >
+      <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '40px' } }} mb="23px" mt="30px">
         Sweat, Smile <br />
         And Repeat
       </Typography>
@@ -43,11 +38,7 @@ const HeroBanner = () => {
       <Typography
         fontWeight={600}
         color="#FF2625"
-        sx={{
-          opacity: '0.1',
-          display: { lg: 'block', xs: 'none' },
-          fontSize: '200px',
-        }}
+        sx={{ opacity: '0.1', display: { lg: 'block', xs: 'none' }, fontSize: '200px' }}
       >
         Exercise
       </Typography>
