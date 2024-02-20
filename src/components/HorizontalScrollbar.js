80d59a58bcd909cf04bdc8e025fb4348102f2cd2
esplanadeA <anna.li.dev@gmail.com>
2024-02-20 22:45:11 +1300

    scroller being horizontal now

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
 import { Box, Typography } from '@mui/material';
 import ExerciseCard from './ExerciseCard';
-
+import 'react-horizontal-scrolling-menu/dist/styles.css';
 // import ExerciseCard from './ExerciseCard';
 import BodyPart from './BodyPart';
 import RightArrowIcon from '../assets/icons/right-arrow.png';
@@ -10,37 +10,39 @@ import LeftArrowIcon from '../assets/icons/left-arrow.png';
 
 // arrows from material
 const LeftArrow = () => {
-  const { scrollPrev } = useContext(VisibilityContext);
+  const { scrollPrev, isFirstItemVisible } = useContext(VisibilityContext);
 
   return (
-    <Typography onClick={() => scrollPrev()} className="right-arrow">
-      <img src={LeftArrowIcon} alt="right-arrow" />
+    <Typography disabled={isFirstItemVisible} onClick={scrollPrev} className="left-arrow">
+      <img src={LeftArrowIcon} alt="left-arrow" />
     </Typography>
   );
 };
 
 const RightArrow = () => {
-  const { scrollNext } = useContext(VisibilityContext);
-
+  const { scrollPrev, isLastItemVisible } = useContext(VisibilityContext);
   return (
-    <Typography onClick={() => scrollNext()} className="left-arrow">
+    <Typography disabled={!isLastItemVisible} onClick={scrollPrev} className="right-arrow">
       <img src={RightArrowIcon} alt="right-arrow" />
     </Typography>
   );
 };
 
 const HorizontalScrollbar = ({ data, bodyParts, setBodyPart, bodyPart }) => (
-  <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
-    {data.map((item) => {
-      <Box key={item.id || item} itemId={item.id || item} title={item.id || item} m="0 40px">
-        {bodyParts ? (
-          <BodyPart item={item} setBodyPart={setBodyPart} bodyPart={bodyPart} />
-        ) : (
-          <ExerciseCard exercise={item} />
-        )}
-      </Box>;
-    })}
-  </ScrollMenu>
+  <div style={{ overflowX: 'auto', whiteSpace: 'nowrap' }}>
+    <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
+      {data.map((item) => (
+        // console.log(item);
+        <Box key={item.id || item} itemID={item.id || item} title={item.id || item} m="0 40px">
+          {bodyParts ? (
+            <BodyPart item={item} setBodyPart={setBodyPart} bodyPart={bodyPart} />
+          ) : (
+            <ExerciseCard exercise={item} />
+          )}
+        </Box>
+      ))}
+    </ScrollMenu>
+  </div>
 );
 
 export default HorizontalScrollbar;
